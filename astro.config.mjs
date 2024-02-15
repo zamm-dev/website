4c6aa7569d94a144b75b928d24dab601ce654f11
Amos Jun-yeung Ng <me@amos.ng>
2024-02-15 17:38:01 +1100

    Update canonical site address

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://zamm.dev',
   integrations: [mdx(), sitemap(), svelte()]
 });
