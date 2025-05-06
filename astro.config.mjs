9d847908c5bd590a3c69155bfe4b9249b913b80a
Amos Jun-yeung Ng <me@amos.ng>
2025-05-06 12:21:37 +0700

    Remove unused Svelte dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zamm.dev',
-  integrations: [mdx(), sitemap(), svelte()]
+  integrations: [mdx(), sitemap()]
 });
