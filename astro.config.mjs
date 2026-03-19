9721017f14eb92dc5bad6c54ba4a1fe2f0b615e3
Amos Jun-yeung Ng <github@amos.ng>
2026-03-20 02:13:20 +0700

    Update with some clarifications
    Add footnote support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import remarkGfm from 'remark-gfm';
 import sitemap from '@astrojs/sitemap';
 
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zamm.dev',
-  integrations: [mdx(), sitemap()]
+  integrations: [mdx({ remarkPlugins: [remarkGfm] }), sitemap()]
 });
